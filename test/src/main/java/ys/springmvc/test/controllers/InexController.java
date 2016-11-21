f279e12ecf5b31591f4f9a4f1df8510d4e55d09a
shuangyao <shuangyao@sohu-inc.com>
2016-11-21 14:19:15 +0800

    update

--- test/src/main/java/ys/springmvc/test/controllers/InexController.java
+++ test/src/main/java/ys/springmvc/test/controllers/InexController.java
@@ -3,7 +3,11 @@ package ys.springmvc.test.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ys.springmvc.test.utils.ImageCodeUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -16,6 +20,17 @@ public class InexController {
     public String index(ModelMap modelMap){
         modelMap.addAttribute("time", System.currentTimeMillis());
         return "index";
+    }
+
+    @RequestMapping("/imgcode")
+    public void imgcode(HttpServletRequest request,
+                          HttpServletResponse response) {
+        try {
+            byte[] bytes = ImageCodeUtils.generateImageCode();
+            response.getOutputStream().write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- test/src/main/java/ys/springmvc/test/controllers/UserController.java
+++ test/src/main/java/ys/springmvc/test/controllers/UserController.java
@@ -1,5 +1,6 @@
 package ys.springmvc.test.controllers;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,10 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ys.springmvc.test.constants.DataSourceConstant;
 import ys.springmvc.test.model.User;
 import ys.springmvc.test.service.UserService;
 import ys.springmvc.test.utils.DataSourceHolder;
+import ys.springmvc.test.utils.JsonResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.OutputStream;
@@ -53,6 +58,13 @@ public class UserController{
 //        model.addAttribute("uname", user.getUname());
         model.addAttribute("user", user);
         return "hello";
+    }
+
+    @RequestMapping(value = "testRedirect", method = RequestMethod.GET)
+    public String testRedirect(RedirectAttributes redirectAttributes) {
+//        redirectAttributes.addAttribute("id", 1);
+        redirectAttributes.addFlashAttribute("id", 1);
+        return "redirect:/user/getById";
     }
 
     @RequestMapping(value = "testDynamicDB", method = RequestMethod.GET)
